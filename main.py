0d8aa252424643fa616eb3c2e67d5382840155ec
Ghazimuhammad <muhammad.taqiyuddin11@ui.ac.id>
2023-09-11 11:52:59 +0700

    .

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import login
+from login import Login
 from PyQt5 import uic
 from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton
 import sys
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    window = login.Login()
+    window = Login()
     app.exec_()
