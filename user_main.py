87ea67bf04ec7169233e69ea228c7d97f1da2d67
Ghazimuhammad <muhammad.taqiyuddin11@ui.ac.id>
2023-09-11 11:09:11 +0700

    rapihin user_main

--- user_main.py
+++ user_main.py
@@ -10,34 +10,37 @@ class UserMain(QMainWindow):
 
         self.main_page = uic.loadUi('file_ui/user_main_menu.ui', self)
 
-        self.label = self.findChild(QLabel, 'logo')
-        pixmap = QPixmap('Pictures/image 2.png')
-        self.label.setPixmap(pixmap)
-        self.label.setScaledContents(True)
-
-        self.button_menu1 = self.findChild(QPushButton, 'push_sparepart')
-        self.button_menu1.setIcon(QIcon("Pictures/sparepart.png"))
-        self.button_menu1.setIconSize(QSize(65, 65))
-
-        self.button_menu2 = self.findChild(QPushButton, 'push_reservation')
-        self.button_menu2.setIcon(QIcon("Pictures/reservation.png"))
-        self.button_menu2.setIconSize(QSize(90, 95))
-
-        self.button_menu3 = self.findChild(QPushButton, 'push_service_list')
-        self.button_menu3.setIcon(QIcon("Pictures/list.png"))
-        self.button_menu3.setIconSize(QSize(65, 65))
         
+        self.set_icon()
 
         self.create_account = self.findChild(QPushButton, 'push_sparepart')
         self.create_account.clicked.connect(self.to_sparepart)
 
         self.show()
-    
+
     def to_sparepart(self):
         self.main_page.close()
         self.sparepart_page = Sparepart()
         self.sparepart_page.show()
+    
+    def set_icon(self):
+        self.label = self.findChild(QLabel, 'logo')
+        pixmap = QPixmap('Pictures/image 2.png')
+        self.label.setPixmap(pixmap)
+        self.label.setScaledContents(True)
 
+        self.push_sparepart = self.findChild(QPushButton, 'push_sparepart')
+        self.push_sparepart.setIcon(QIcon("Pictures/sparepart.png"))
+        self.push_sparepart.setIconSize(QSize(65, 65))
+
+        self.push_reservation = self.findChild(QPushButton, 'push_reservation')
+        self.push_reservation.setIcon(QIcon("Pictures/reservation.png"))
+        self.push_reservation.setIconSize(QSize(90, 95))
+
+        self.push_service_list = self.findChild(QPushButton, 'push_service_list')
+        self.push_service_list.setIcon(QIcon("Pictures/list.png"))
+        self.push_service_list.setIconSize(QSize(65, 65))
+    
 class Sparepart(QMainWindow):
     def __init__(self):
         super(Sparepart, self).__init__()
@@ -47,8 +50,6 @@ class Sparepart(QMainWindow):
         self.label = self.findChild(QLabel, 'logo_sparepart')
         pixmap = QPixmap('Pictures/sparepart.png')
         self.label.setPixmap(pixmap)
-        # self.label.setStyleSheet("background-color: #2D2E43")
-        # self.setStyleSheet("background-color: #2D2E43") 
         self.label.setScaledContents(True)
 
         self.push_back = self.findChild(QPushButton, 'push_back')
